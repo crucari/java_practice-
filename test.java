0c9f0caef7842b9f0247be4e29000a8bfc1dbee9
Carina Cruz <lecruz@bu.edu>
2018-10-11 23:42:11 +0000

    opening a web progress in progress

---
+++ test.java
@@ -0,0 +1,19 @@
+package test; 
+
+import org.openqa.selenium.WebDriver;
+Import org.openqa.selenium.firefox.FirefoxDriver; 
+
+public class FirstSeleniumTest {
+     
+      Public static void main(String[] args) {
+
+      WebDriver driver = new FirefoxDriver(); 
+
+       driver.get("http://seleniumhq.org"); 
+
+        }
+
+}
+
+
+           
